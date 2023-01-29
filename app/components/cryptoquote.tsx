873263cc34b66d98c9b94dd1f12bbff2d17547ac
Steven Pingel <strikeralley@gmail.com>
2023-01-29 02:09:41 -0600

    Some more shit

--- app/components/cryptoquote.tsx
+++ app/components/cryptoquote.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, createRef, useState } from "react";
 import { alphaRegex, splitToAlphas } from "~/common/utlities";
 import LetterInput from "./letterInput";
 import StaticCharacter from "./staticCharacter";
@@ -16,51 +16,73 @@ const Cryptoquote = ({ cryptoquote, author }: CryptoquoteProps) => {
     alphas.map(entry => initialKey.has(entry) || initialKey.set(entry, ''));
 
     const [cryptoKey, setCryptoKey] = useState<Map<string, string>>(initialKey);
-
-    const handleKeyUp = (e: KeyboardEvent) => {
-        if (e.key.length == 1 && e.key.match(alphaRegex)) {
-            const emptyFields: HTMLInputElement | null = document.querySelector(`input[value=""]`);
-            if (emptyFields) {
-                emptyFields.focus();
-            }
-        }
-    }
+    const [currentIndex, setActiveIndex] = useState<number>(0);
+    const [inputRefsArray] = useState<React.RefObject<HTMLInputElement>[]>(() =>
+        Array.from({ length: alphas.length }, () => createRef())
+    );
 
     useEffect(() => {
+        if (inputRefsArray?.[0]?.current) {
+            inputRefsArray?.[0]?.current?.focus();
+        }
+
+        const handleKeyUp = (e: KeyboardEvent) => {
+            if (e.key.length == 1 && e.key.match(alphaRegex)) {
+                setActiveIndex((prevIndex) => {
+                    const index = prevIndex ?? 0;
+                    let nextIndex = index < alphas.length -1 ? index + 1 : 0;
+                    while(nextIndex < alphas.length - 1){
+                        const nextInput = inputRefsArray?.[nextIndex]?.current;
+                        if(!nextInput?.value.match(alphaRegex)){
+                            nextInput?.focus();
+                            nextInput?.select();
+                            break;
+                        }
+                        nextIndex++
+                    }
+                    
+                    return nextIndex;
+                });
+            }
+        }
+
         document.addEventListener('keyup', handleKeyUp);
 
         return () => {
             document.removeEventListener('keyup', handleKeyUp);
         }
-    })
+    }, [alphas.length, inputRefsArray])
 
     const onCharacterChange = (cryptoLetter: string, letter: string) => {
         setCryptoKey(key => new Map(key.set(cryptoLetter, letter)));
     }
 
-    const createWordComponents = (word: string) => {
-        const words = word.split('').map((char, index) => {
+    let refIndex = 0;
+    const createWordComponent = (word: string) => {
+        const words = word.split('').map((char) => {
             if (char.match(alphaRegex)) {
-                return <LetterInput key={`${char}-${index}`} cryptoLetter={char} onChange={onCharacterChange} cryptoKey={cryptoKey} />;
+                const index = refIndex++;
+                const result = <LetterInput active={index === currentIndex} onFocus={() => setActiveIndex(index)} ref={inputRefsArray[index]} key={`${word}-${char}-${index}`} cryptoLetter={char} onChange={onCharacterChange} cryptoKey={cryptoKey} />;
+                return result;
             }
 
-            return <StaticCharacter key={`static-${index}`} character={char} />
+            return <StaticCharacter key={`static-${word}`} character={char} />
 
         });
 
         return (
-            <>
+            <span key={`${refIndex}-${word}`}>
                 <div className="inline-block">
                     {words}
                 </div>
                 <WhiteSpace key={`ws-${word}`} />
-            </>
+            </span>
         )
     }
 
     const wordComponents = cryptoquote
         .trim().split(' ')
-        .map(word => createWordComponents(word));
+        .map(word => createWordComponent(word));
 
 
     return (
--- app/components/letterInput.tsx
+++ app/components/letterInput.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { forwardRef } from "react";
 
 interface LetterInputProps {
     cryptoLetter: string
     cryptoKey: Map<string, string>
     onChange: (cryptoLetter:string, letter:string) => void
+    onFocus: () => void
+    active: boolean
 }
 
-const LetterInput = ({cryptoLetter, cryptoKey, onChange}:LetterInputProps) => {
+const LetterInput = forwardRef(({cryptoLetter, cryptoKey, onChange, onFocus, active}:LetterInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
     const normalizedCryptoLetter = cryptoLetter.toUpperCase();
-    const [isActive, setIsActive] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -22,19 +23,20 @@ const LetterInput = ({cryptoLetter, cryptoKey, onChange}:LetterInputProps) => {
     };
 
     const handleFocus = (e: React.FocusEvent) => {
-        setIsActive(true);
+        onFocus();
         clearInput();
     }
 
-
     return (
         <div className="w-10 h-20 my-4 min-h-full mx-0.5 text-center inline-block border-2 border-black">
-            <input name='character' type='text' onFocus={handleFocus} onBlur={() => setIsActive(false)} maxLength={1} onChange={handleChange} value={cryptoKey.get(normalizedCryptoLetter)} className={`w-9 h-1/2 ${isActive && 'bg-yellow-300'} text-center inline-block outline-none`} autoComplete="off"/>
+            <input ref={ref} name='character' type='text' maxLength={1} onFocus={handleFocus} onChange={handleChange} value={cryptoKey.get(normalizedCryptoLetter)} className={`w-9 h-1/2 ${active && 'bg-yellow-300'} text-center inline-block outline-none caret-transparent`} autoComplete="off"/>
             <div className="h-1/2 pt-2 bg-black text-white">
                 {normalizedCryptoLetter}
             </div>
         </div>
     );
-}
+});
+
+LetterInput.displayName = 'LetterInput';
 
 export default LetterInput;
